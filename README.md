67b33437649e3b4d1aec7442ef043f8b563e9f60
divcamillediv <camille.divisia@hotmail.com>
2025-11-28 23:22:47 +0100

    readme

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# fraud-detector
-fraud-detector EGP
+# FraudGuard AI
+Détecteur de fraudes de transactions en ligne en tant réel.
+
+## Besoins fonctionnels
+
+Un utilisateur doit:
+[x] Visualiser les alertes récentes en live
+[x] Voir les détails d'une alerte
+[x] Modifier les paramètres du logiciel
+[x] Marquer une nouvelle alerte comme "Fraude" ou "Faux positif"
--- backend/main.py
+++ backend/main.py
@@ -14,9 +14,11 @@ import numpy as np
 
 # --- CONFIGURATION ---
 # Remplacer par vos clés Supabase (disponibles dans Project Settings > API)
-SUPABASE_URL = os.getenv("SUPABASE_URL")
-SUPABASE_KEY = os.getenv("SUPABASE_ANON_KEY")
+#SUPABASE_URL = os.getenv("SUPABASE_URL")
+#SUPABASE_KEY = os.getenv("SUPABASE_ANON_KEY")
 
+SUPABASE_URL = "https://ecnycudjtekhfgbzglqu.supabase.co"
+SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVjbnljdWRqdGVraGZnYnpnbHF1Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc2MzQ4ODQ3MSwiZXhwIjoyMDc5MDY0NDcxfQ.ojJe2KN6woZRb78QxP-0O0qoeaF-g4Q-o-7eqwBVPkU"  # Remplacez par votre clé Supabase
 
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 app = FastAPI(title="Fraud Detection AI Engine")
